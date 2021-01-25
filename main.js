80cf1f31481348d3394828cd52fe23be4240be13
hyoeun93 <hyoeun93@gmail.com>
2021-01-25 17:19:39 -0500

    improve the performance

--- main.js
+++ main.js
@@ -18,32 +18,44 @@ function onAdd() {
   inputText.focus();
 }
 
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'list__name');
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="trash">
+          <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+  `;
+  id++;
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const span = document.createElement('span');
-  span.setAttribute('class', 'item__name');
-  span.innerText = text;
+  // const span = document.createElement('span');
+  // span.setAttribute('class', 'item__name');
+  // span.innerText = text;
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'trash');
-  deleteBtn.innerHTML = ' <i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    list.removeChild(itemRow);
-  });
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'trash');
+  // deleteBtn.innerHTML = ' <i class="fas fa-trash-alt"></i>';
+  // deleteBtn.addEventListener('click', () => {
+  //   list.removeChild(itemRow);
+  // });
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item__divider');
 
-  item.appendChild(span);
-  item.appendChild(deleteBtn);
+  // item.appendChild(span);
+  // item.appendChild(deleteBtn);
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
   return itemRow;
 }
 
@@ -54,5 +66,13 @@ addBtn.addEventListener('click', () => {
 inputText.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
+  }
+});
+
+list.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.list__name[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
